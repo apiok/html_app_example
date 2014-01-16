8f481989380711a1d9202de70c8d4cf410694153
artyom.lobachev <artyom.lobachev@odnoklassniki.ru>
2014-01-16 08:59:50 +0400

    Now using FAPI.Util.getRequestParameters()

--- ru/script.js
+++ ru/script.js
@@ -14,7 +14,7 @@ FAPI.init(rParams["api_server"], rParams["apiconnection"],
           */
           function() {
               initCard();
-              currentUserId = getUrlParameters("logged_user_id","",false);
+              currentUserId = FAPI.Util.getRequestParameters()["logged_user_id"];
           },
           /*
           * Второй параметр:
@@ -115,29 +115,4 @@ function fillCard(userInfo){
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function getUrlParameters(parameter, staticURL, decode) {
-   /*
-    Function: getUrlParameters
-    Description: Get the value of URL parameters either from 
-                 current URL or static URL
-    Author: Tirumal
-    URL: www.code-tricks.com
-   */
-   var currLocation = (staticURL.length)? staticURL : window.location.search,
-       parArr = currLocation.split("?")[1].split("&"),
-       returnBool = true;
-   
-   for(var i = 0; i < parArr.length; i++){
-        parr = parArr[i].split("=");
-        if(parr[0] == parameter){
-            return (decode) ? decodeURIComponent(parr[1]) : parr[1];
-            returnBool = true;
-        }else{
-            returnBool = false;            
-        }
-   }
-   
-   if(!returnBool) return false;
 }
