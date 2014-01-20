af3a8e112ea3f6fba3b6ea15933217fe189cc72b
root <root@deaddev.com>
2014-01-20 11:03:38 -0500

    Englis version only. Now logging payment operations with error_log().

--- en/payment.php
+++ en/payment.php
@@ -1,8 +1,15 @@
 <?php
+// simpliest, but not best way to save transaction info
+$params = "";
+foreach($_GET as $key=>$value){
+	$params = $params.$key." = ".$value."; ";
+}
+// error_log("New payment: ".$params."\n", 3, "/www/artem.deaddev.com/log.log");
+error_log("New payment: ".$params."\n", 3, "/var/log/php/payment_log.log");
 //TODO: check and save transaction info
 // adding header
 header('Content-Type: application/xml');
-//creating xml document
+// creating xml document
 $dom = new DomDocument('1.0'); 
 
 //adding root tag - <callbacks_payment_response> 
