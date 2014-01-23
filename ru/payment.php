4fa9ade47ad8f1740c6c58ebee93819f48237bcc
artyom.lobachev <artyom.lobachev@odnoklassniki.ru>
2014-01-23 16:37:22 +0400

    Fixed link to auth docs

--- ru/payment.php
+++ ru/payment.php
@@ -30,7 +30,7 @@ class Payment {
     );
     
     // функция рассчитывает подпись для пришедшего запроса
-    // подробнее про алгоритм расчета подписи можно посмотреть в документации (http://apiok.ru/wiki/pages/viewpage.action?pageId=46137373#API%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D0%B8%D1%8F%28%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9%29-%D0%90%D1%83%D1%82%D0%B5%D0%BD%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D1%8F%D0%B8%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F)
+    // подробнее про алгоритм расчета подписи можно посмотреть в документации (http://apiok.ru/wiki/pages/viewpage.action?pageId=42476522)
     public static function calcSignature($request){
         $tmp = $request;
         unset($tmp["sig"]);
